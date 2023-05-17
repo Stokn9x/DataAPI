1943974a87810f009011bdf896fd44ffb5604f20
Casper <capper2704@hotmail.dk>
2023-05-17 12:45:31 +0200

    Implemented a try except

--- data.py
+++ data.py
@@ -1,17 +1,20 @@
 
 def spread_id(channel_id):
+    try:
+        if channel_id == 1107565551766745139: #Zer0
+            return '1l2aXE0Lu3vcOXoFXKyiN6xrpofRsJrKyZpSHHFUNGOg'
 
-    if channel_id == 1107565551766745139: #Zer0
-        return '1l2aXE0Lu3vcOXoFXKyiN6xrpofRsJrKyZpSHHFUNGOg'
+        elif channel_id == 1107565600647172146: #K1ngWave
+            return '11hUQnDVInBjS744s842yVJCPYeDpyymg79CgLrfPshE'
 
-    elif channel_id == 1107565600647172146: #K1ngWave
-        return '11hUQnDVInBjS744s842yVJCPYeDpyymg79CgLrfPshE'
+        elif channel_id == 1107566039014838302: #Monner
+            return '1CL7DL68aB9VjI94f3g33kJ7W9wzGDE8YsruW2g9kLAQ'
 
-    elif channel_id == 1107566039014838302: #Monner
-        return '1CL7DL68aB9VjI94f3g33kJ7W9wzGDE8YsruW2g9kLAQ'
+        elif channel_id == 1107573013651660820: #AB
+            return '1IRv0QicOU044COgTk0ACK__u8EjOhim3lnfRZthdlaM'
 
-    elif channel_id == 1107573013651660820: #AB
-        return '1IRv0QicOU044COgTk0ACK__u8EjOhim3lnfRZthdlaM'
+        elif channel_id == 1103348691466723399: #Cepter
+            return '1HuKfTI0KKPvdlxY5MX9BIzlSExS2kLzJ7BdW739vbKQ'
 
-    elif channel_id == 1103348691466723399: #Cepter
-        return '1HuKfTI0KKPvdlxY5MX9BIzlSExS2kLzJ7BdW739vbKQ'
+    except:
+        print("channel not working")
